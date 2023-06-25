b9e1bec18a204aa9daa6476c484ef8251a4ab3b1
ziadalsafe <zalsafe4@gmail.com>
2023-06-25 17:31:31 +0300

    fis issue

--- controllers/password/forgotPasswordController.js
+++ controllers/password/forgotPasswordController.js
@@ -12,45 +12,47 @@ const forgotPasswordController = handler(async (req, res) => {
   UNION ALL
   SELECT password,email
   FROM teacher WHERE email = '${req.body.email}'`,)
-  if (!user) {
+  // res.send(user)
+  if (user.rowCount == 0) {
     return res.status(404).send("users not found");
-  }
-  const email = user.rows[0].email;
-  const passowed = user.rows[0].passowed
+  } else {
+    const email = user.rows[0].email;
+    const passowed = user.rows[0].passowed
 
-  const secret = process.env.SECRETKEY + passowed;
-  const token = jwt.sign({ email }, secret);
+    const secret = process.env.SECRETKEY + passowed;
+    const token = jwt.sign({ email }, secret);
 
-  const link = `http://localhost:3000/reset-password/${email}/${token}`;
-  const sentToemail = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
+    const link = `http://localhost:3000/reset-password/${email}/${token}`;
+    const sentToemail = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASS,
+      },
+    });
+    const options = {
       user: process.env.USER_EMAIL,
-      pass: process.env.USER_PASS,
-    },
-  });
-  const options = {
-    user: process.env.USER_EMAIL,
-    to: email,
-    subject: "Reset Password Link",
-    html: `<div>
+      to: email,
+      subject: "Reset Password Link",
+      html: `<div>
     <h4>
     Click on the link below to reset the passowed
     <p>${link}</p>
     </h4>
     </div>`,
-  };
-  sentToemail.sendMail(options, function (error, success) {
-    if (error) {
-      console.log(error);
-      res.status(500).json(error);
-    } else {
-      console.log("Email send " + success.response);
-      res.render("link-send");
-    }
-  });
-  res.status(200).send({ massege: "Check your email" })
-  //  res.json({ message: "Click on the link ", resetPasswordLink: link });
+    };
+    sentToemail.sendMail(options, function (error, success) {
+      if (error) {
+        console.log(error);
+        res.status(500).json(error);
+      } else {
+        console.log("Email send " + success.response);
+        res.render("link-send");
+      }
+    });
+    res.status(200).send({ massege: "Check your email" })
+    //  res.json({ message: "Click on the link ", resetPasswordLink: link });
+  }
 });
 
 
