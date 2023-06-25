dddcfd03e73ed16316484aab3dea0877fcb8e3a2
ziadalsafe <zalsafe4@gmail.com>
2023-06-25 17:09:50 +0300

    forgot the password

--- controllers/password/forgotPasswordController.js
+++ controllers/password/forgotPasswordController.js
@@ -5,77 +5,60 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 
 
-
 const forgotPasswordController = handler(async (req, res) => {
   const user = await client.query(`
   SELECT password,email
   FROM students WHERE email = '${req.body.email}'
   UNION ALL
   SELECT password,email
-  FROM teacher WHERE email = '${req.body.email}'`,
-    (error, result) => {
-      if (error) {
-        res.status(400);
-        res.json(error);
-      } else if (result.rows.length < 1) {
-        res.status(404);
-        res.json({ message: "email not found" })
-      } else {
-        res.status(200);
-        res.json(result.rows)
-      }
-      const secret = process.env.SECRETKEY + result.rows.password;
-      const token = jwt.sign({ email: result.rows.email }, secret);
+  FROM teacher WHERE email = '${req.body.email}'`,)
+  if (!user) {
+    return res.status(404).send("users not found");
+  }
+  const email = user.rows[0].email;
+  const passowed = user.rows[0].passowed
 
+  const secret = process.env.SECRETKEY + passowed;
+  const token = jwt.sign({ email }, secret);
 
-      const link = `http://localhost:4500/reset-password/${result.rows.email}/${token}`;
-
-
-      const sentToemail = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.USER_EMAIL,
-          pass: process.env.USER_PASS,
-        },
-      });
-      const options = {
-        user: process.env.USER_EMAIL,
-        to: result.rows.email,
-        subject: "Reset Password Link",
-        html: `<div>
-          <h4>
-          Click on the link below to reset the passowed
-          <p>${link}</p>
-          </h4>
-          </div>`,
-      };
-      sentToemail.sendMail(options, function (error, success) {
-        if (error) {
-          console.log(error);
-          res.status(500).json(error);
-        } else {
-          console.log("Email send " + success.response);
-          res.render("link-send");
-        }
-      });
-
-      res.json({ message: "Click on the link ", resetPasswordLink: link });
-
-
-
-
-
-
-
-
-
-    })
-
-
-
-
-
+  const link = `http://localhost:3000/reset-password/${email}/${token}`;
+  const sentToemail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASS,
+    },
+  });
+  const options = {
+    user: process.env.USER_EMAIL,
+    to: email,
+    subject: "Reset Password Link",
+    html: `<div>
+    <h4>
+    Click on the link below to reset the passowed
+    <p>${link}</p>
+    </h4>
+    </div>`,
+  };
+  sentToemail.sendMail(options, function (error, success) {
+    if (error) {
+      console.log(error);
+      res.status(500).json(error);
+    } else {
+      console.log("Email send " + success.response);
+      res.render("link-send");
+    }
+  });
+  res.status(200).send({ massege: "Check your email" })
+  //  res.json({ message: "Click on the link ", resetPasswordLink: link });
 });
+
+
+
+
+
+
+
 
 // module.exports.getResetPasswordView = handler(async (req, res) => {
 //   const user = await Users.findById(req.params.userId);
